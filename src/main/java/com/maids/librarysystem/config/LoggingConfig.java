3ece8ae8890549183a6b0026ce72007600ea88cd
Mobolajihabib <93858825+Mobolajihabib@users.noreply.github.com>
2024-05-11 23:31:05 +0100

    update

--- src/main/java/com/maids/librarysystem/config/LoggingConfig.java
+++ src/main/java/com/maids/librarysystem/config/LoggingConfig.java
@@ -9,22 +9,24 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 @Slf4j
 public class LoggingConfig {
 
-    @Before("execution(* com.example.controller.*.*(..))")
+    @Before("execution(* com.maids.librarysystem.service.*.*(..))")
     public void logMethodEntry(JoinPoint joinPoint) {
-        log.info("Entering method: " + joinPoint.getSignature().getName());
+        log.info("Entering method: " + joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
     }
 
-    @AfterReturning(pointcut = "execution(* com.example.controller.*.*(..))", returning = "result")
+    @AfterReturning(pointcut = "execution(* com.maids.librarysystem.service.*.*(..))", returning = "result")
     public void logMethodExit(JoinPoint joinPoint, Object result) {
         log.info("Exiting method: " + joinPoint.getSignature().getName() + ", Result: " + result);
     }
 
-    @AfterThrowing(pointcut = "execution(* com.example.controller.*.*(..))", throwing = "exception")
+    @AfterThrowing(pointcut = "execution(* com.maids.librarysystem.service.*.*(..))", throwing = "exception")
     public void logException(JoinPoint joinPoint, Throwable exception) {
         log.error("Exception in method: " + joinPoint.getSignature().getName() + ", Message: " + exception.getMessage());
     }
